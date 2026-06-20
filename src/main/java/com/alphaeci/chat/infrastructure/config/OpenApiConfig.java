7c82f14cc87c010d8ce573200be041e92c3a1f4f
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-06-19 22:22:26 -0500

    Feat: Implementacion Config

---
+++ src/main/java/com/alphaeci/chat/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.alphaeci.chat.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Chat Service API")
+                        .description("AlphaECI — PATRICI.A Chat Microservice")
+                        .version("1.0.0"));
+    }
+}
